38fe0fefebf3467c0106207a36ac1f7c25a4868c
Enzine <sonja.makipaja@kolumbus.fi>
2016-02-28 00:10:56 +0200

    Update ViestiDao.java
    -sonja päivitti

--- foorumi/src/main/java/tkpe/foorumi/ViestiDao.java
+++ foorumi/src/main/java/tkpe/foorumi/ViestiDao.java
@@ -1,23 +1,55 @@
 package tkpe.foorumi;
 
-import java.sql.SQLException;
-import java.util.List;
+import java.sql.*;
+import java.util.*;
+import tkpe.foorumi.*;
 
-public class ViestiDao implements Dao<Object, Object>{
+public class ViestiDao implements Dao<Viesti, Integer>{
+    private Database database;
 
-    @Override
-    public Object findOne(Object key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ViestiDao(Database database) {
+        this.database = database;
     }
 
     @Override
-    public List<Object> findAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public Viesti findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Viesti WHERE id = ?");
+        stmt.setObject(1, key);
 
-    @Override
-    public void delete(Object key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        int id = rs.getInt("id");
+        String sisalto = rs.getString("sisalto");
+        Timestamp aikaleima = rs.getTimestamp("aikaleima");
+        String kirjoittaja = rs.getString("kirjoittaja");
+        Lanka lanka = rs.getObject("lanka", Lanka);
+
+       Viesti v = new Viesti(id, sisalto, aikaleima, kirjoittaja, lanka);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return v;
     }
     
+    @Override
+    public List<Viesti> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Viesti> findAllIn(Collection<Integer> keys) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
