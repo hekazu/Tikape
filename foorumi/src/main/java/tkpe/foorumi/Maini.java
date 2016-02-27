f24e2092f83c71baedcabc2bbb60e1b883562942
Enzine <sonja.makipaja@kolumbus.fi>
2016-02-28 00:09:57 +0200

    Update Maini.java
    -sonja päivitti

--- foorumi/src/main/java/tkpe/foorumi/Maini.java
+++ foorumi/src/main/java/tkpe/foorumi/Maini.java
@@ -1,9 +1,13 @@
-
 package tkpe.foorumi;
 
 public class Maini {
+
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:foorumi.db");
-        
+        AlueDao adao = new AlueDao(database);
+
+        Alue alue = adao.findOne(1);
+        System.out.println(alue.getId()
+                + " " + alue.getNimi());  //ei toimi
     }
 }
