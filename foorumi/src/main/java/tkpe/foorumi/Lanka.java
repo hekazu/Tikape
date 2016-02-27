9fad33fc06c22b3229d34f4d0c88f67a129f9697
Enzine <sonja.makipaja@kolumbus.fi>
2016-02-28 00:08:45 +0200

    Update Lanka.java
    -sonja päivitti

--- foorumi/src/main/java/tkpe/foorumi/Lanka.java
+++ foorumi/src/main/java/tkpe/foorumi/Lanka.java
@@ -1,14 +1,36 @@
 package tkpe.foorumi;
 
+import java.util.*;
+
 public class Lanka {
+
     public final int id;
     private String otsikko;
     private Alue alue;
+    private List<Viesti> viestit;
+
+    public Lanka(int id, String otsikko) {
+        this.id = id;
+        this.otsikko = otsikko;
+    }
 
     public Lanka(int id, String otsikko, Alue alue) {
         this.id = id;
         this.otsikko = otsikko;
         this.alue = alue;
+    }
+
+    public Lanka(int id, String otsikko, List<Viesti> viestit) {
+        this.id = id;
+        this.otsikko = otsikko;
+        this.viestit = viestit;
+    }
+
+    public Lanka(int id, String otsikko, Alue alue, List<Viesti> viestit) {
+        this.id = id;
+        this.otsikko = otsikko;
+        this.alue = alue;
+        this.viestit = viestit;
     }
 
     public int getId() {
@@ -21,7 +43,23 @@ public class Lanka {
 
     public Alue getAlue() {
         return alue;
-    }   
+    }
+
+    public List<Viesti> getViestit() {
+        return viestit;
+    }
+
+    public void setAlue(Alue alue) {
+        this.alue = alue;
+    }
+
+    public void lisaaViesti(Viesti v) {
+        viestit.add(v);
+    }
+
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
+    }
 }
 
 
