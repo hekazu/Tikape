40a27a3a1b437657ee03650ca4f5015fcddec708
Enzine <sonja.makipaja@kolumbus.fi>
2016-02-28 00:06:58 +0200

    Update AlueDao.java
    - sonja päivitti

--- foorumi/src/main/java/tkpe/foorumi/AlueDao.java
+++ foorumi/src/main/java/tkpe/foorumi/AlueDao.java
@@ -1,23 +1,52 @@
 package tkpe.foorumi;
 
-import java.sql.SQLException;
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
-public class AlueDao implements Dao<Object, Object>{
+public class AlueDao implements Dao<Alue, Integer> {
+
+    private Database database;
+
+    public AlueDao(Database database) {
+        this.database = database;
+    }
 
     @Override
-    public Object findOne(Object key) throws SQLException {
+    public Alue findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue WHERE nimi = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        int id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        Alue a = new Alue(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return a;
+    }
+
+    @Override
+    public List<Alue> findAll() throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<Object> findAll() throws SQLException {
+    public void delete(Integer key) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void delete(Object key) throws SQLException {
+    public List<Alue> findAllIn(Collection<Integer> keys) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
