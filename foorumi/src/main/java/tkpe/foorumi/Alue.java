ad5767aca1a6047e88aeaa52da46b40e4757e9cc
Enzine <sonja.makipaja@kolumbus.fi>
2016-02-28 00:05:52 +0200

    Update Alue.java
    - sonja päivitti

--- foorumi/src/main/java/tkpe/foorumi/Alue.java
+++ foorumi/src/main/java/tkpe/foorumi/Alue.java
@@ -1,12 +1,22 @@
 package tkpe.foorumi;
 
+import java.util.*;
+
 public class Alue {
+
     private final int id;
     private String nimi;
+    private List<Lanka> langat;
 
     public Alue(int id, String nimi) {
         this.id = id;
         this.nimi = nimi;
+    }
+
+    public Alue(int id, String nimi, List<Lanka> langat) {
+        this.id = id;
+        this.nimi = nimi;
+        this.langat = langat;
     }
 
     public int getId() {
@@ -16,5 +26,12 @@ public class Alue {
     public String getNimi() {
         return nimi;
     }
-      
+
+    public List<Lanka> getLangat() {
+        return langat;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }    
 }
