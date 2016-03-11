a45e404a1b9cd96058644b08d5985538553f005a
Enzine <sonja.makipaja@kolumbus.fi>
2016-03-11 22:11:48 +0200

    Update Maini.java

--- foorumi/src/main/java/tkpe/foorumi/Maini.java
+++ foorumi/src/main/java/tkpe/foorumi/Maini.java
@@ -4,10 +4,12 @@ public class Maini {
 
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:foorumi.db");
-        AlueDao adao = new AlueDao(database);
+        AlueDao alueDao = new AlueDao(database);
+        LankaDao lankaDao = new LankaDao(database, alueDao);
+        ViestiDao viestiDao = new ViestiDao(database, lankaDao);
 
-        Alue alue = adao.findOne(1);
-        System.out.println(alue.getId()
-                + " " + alue.getNimi());  //ei toimi
+        Viesti viesti = viestiDao.findOne(4);
+        System.out.println("ID: " + viesti.getId() + ". Kirjoittaja: " + viesti.getKirjoittaja() + " " + viesti.getAikaleima() + ", Viesti: " + viesti.getSisalto());
+        // toimii muilla luokilla vastaava paitsi täällä ViestiDaolla. Ongelma on ViestiDao:n findOne-metodin TimeStamp-oliossa jollain tasolla.
     }
 }
