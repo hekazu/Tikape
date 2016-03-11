a3eb69329f619b75e64cc64851968b95f4a47f4b
Enzine <sonja.makipaja@kolumbus.fi>
2016-03-11 22:10:01 +0200

    Update Database.java

--- foorumi/src/main/java/tkpe/foorumi/Database.java
+++ foorumi/src/main/java/tkpe/foorumi/Database.java
@@ -5,67 +5,13 @@ import java.util.*;
 
 public class Database<T> {
 
-    private String address;
-    private boolean debug;
-    private Connection connection;
+    private String databaseAddress;
 
-    public Database(String address) throws Exception {
-        this.address = address;
-        this.connection = DriverManager.getConnection(address);
+    public Database(String databaseAddress) throws ClassNotFoundException {
+        this.databaseAddress = databaseAddress;
     }
 
     public Connection getConnection() throws SQLException {
-        return connection;
-    }
-    
-    public void setDebugMode(boolean d) {
-        debug = d;
-    }
-    
-    private void debug(ResultSet rs) throws SQLException {
-        int columns = rs.getMetaData().getColumnCount();
-        for (int i = 0; i < columns; i++) {
-            System.out.print(
-                    rs.getObject(i + 1) + ":"
-                    + rs.getMetaData().getColumnName(i + 1) + "  ");
-        }
-
-        System.out.println();
-    }
-    
-    public List<T> queryAndCollect(String query, Collector<T> col) throws SQLException {
-        List<T> rows = new ArrayList<>();
-        Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-
-        while (rs.next()) {
-            if (debug) {
-                System.out.println("---");
-                System.out.println(query);
-                debug(rs);
-                System.out.println("---");
-            }
-
-            rows.add(col.collect(rs));
-        }
-
-        rs.close();
-        stmt.close();
-        return rows;
-    }
-    
-    public int update(String updateQuery) throws SQLException {
-        Statement stmt = connection.createStatement();
-        int changes = stmt.executeUpdate(updateQuery);
-
-        if(debug) {
-            System.out.println("---");
-            System.out.println(updateQuery);
-            System.out.println("Changed rows: " + changes);
-            System.out.println("---");
-        }
-        stmt.close();
-
-        return changes;
+        return DriverManager.getConnection(databaseAddress);
     }
 }
