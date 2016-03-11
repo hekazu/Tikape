2f1fe5dc27aaf4ad8e8b21251b6f5b1eb43df153
Enzine <sonja.makipaja@kolumbus.fi>
2016-03-11 19:04:31 +0200

    Update Lanka.java

--- foorumi/src/main/java/tkpe/foorumi/Lanka.java
+++ foorumi/src/main/java/tkpe/foorumi/Lanka.java
@@ -4,62 +4,45 @@ import java.util.*;
 
 public class Lanka {
 
-    public final int id;
+    public int id;
     private String otsikko;
     private Alue alue;
     private List<Viesti> viestit;
 
-    public Lanka(int id, String otsikko) {
-        this.id = id;
+    public Lanka(String otsikko) {
         this.otsikko = otsikko;
-    }
-
-    public Lanka(int id, String otsikko, Alue alue) {
-        this.id = id;
-        this.otsikko = otsikko;
-        this.alue = alue;
-    }
-
-    public Lanka(int id, String otsikko, List<Viesti> viestit) {
-        this.id = id;
-        this.otsikko = otsikko;
-        this.viestit = viestit;
-    }
-
-    public Lanka(int id, String otsikko, Alue alue, List<Viesti> viestit) {
-        this.id = id;
-        this.otsikko = otsikko;
-        this.alue = alue;
-        this.viestit = viestit;
+        this.viestit = new ArrayList<>();
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public String getOtsikko() {
         return otsikko;
     }
-
-    public Alue getAlue() {
-        return alue;
+    
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
     }
 
     public List<Viesti> getViestit() {
         return viestit;
     }
 
+    public void setViestit(List<Viesti> viestit) {
+        this.viestit = viestit;
+    }
+    
+    public Alue getAlue() {
+        return alue;
+    }
+
     public void setAlue(Alue alue) {
         this.alue = alue;
     }
-
-    public void lisaaViesti(Viesti v) {
-        viestit.add(v);
-    }
-
-    public void setOtsikko(String otsikko) {
-        this.otsikko = otsikko;
-    }
 }
-
-
