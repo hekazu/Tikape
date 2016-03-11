1af1fb2d560b51bf2aff098f4d25836db0583ffc
Enzine <sonja.makipaja@kolumbus.fi>
2016-03-11 19:08:47 +0200

    Update Viesti.java

--- foorumi/src/main/java/tkpe/foorumi/Viesti.java
+++ foorumi/src/main/java/tkpe/foorumi/Viesti.java
@@ -1,39 +1,58 @@
 package tkpe.foorumi;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 
 public class Viesti {
     private int id;
     private String sisalto;
-    private Timestamp aikaleima;
+    private Timestamp aika;
     private String kirjoittaja;
     private Lanka lanka;
 
-    public Viesti(int id, String sisalto, Timestamp aikaleima, String kirjoittaja, Lanka lanka) {
-        this.id = id;
+    public Viesti(String sisalto, String kirjoittaja) {
         this.sisalto = sisalto;
-        this.aikaleima = aikaleima;
+        this.aika = Timestamp.valueOf(LocalDateTime.now());
         this.kirjoittaja = kirjoittaja;
-        this.lanka = lanka;
     }
 
     public int getId() {
         return id;
     }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getSisalto() {
         return sisalto;
     }
+    
+    public void setSisalto(String sisalto) {
+        this.sisalto = sisalto;
+    }
 
-    public Timestamp getAikaleima() {
-        return aikaleima;
+    public Timestamp getAika() {
+        return aika;
+    }
+    
+    public void setAika(Timestamp aika) {
+        this.aika = aika;
     }
 
     public String getKirjoittaja() {
         return kirjoittaja;
     }
+    
+    public void setKirjoittaja(String kirjoittaja) {
+        this.kirjoittaja = kirjoittaja;
+    }
 
     public Lanka getLanka() {
         return lanka;
+    }
+    
+    public void setLanka(Lanka lanka) {
+        this.lanka = lanka;
     }
 }
